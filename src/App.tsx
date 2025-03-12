1c2651de78e8f29749d6e727ad3343653b3bebf0
DfMaciel <104574671+DfMaciel@users.noreply.github.com>
2025-03-11 22:10:35 -0300

    fix: Correção de erros de router duplicados #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { ThemeProvider } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
 import AppRoutes from "@routes/routes";
 import lightTheme from "./lightTheme";
+import Sidebar from "@components/sidebar";
+
+const drawerWidth = 260;
 
 export default function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <AppRoutes />
-    </ThemeProvider>
+      <ThemeProvider theme={lightTheme}>
+        <Box sx={{ display: 'flex' }}>
+          <Sidebar />
+          <Box 
+            component="main" 
+            // sx={{ 
+            //   flexGrow: 1, 
+            //   p: 3, 
+            //   marginLeft: { xs: 0, md: `${drawerWidth}px` },
+            //   width: { xs: '100%', md: `calc(100% - ${drawerWidth}px)` }
+            // }}
+          >
+            <AppRoutes />
+          </Box>
+        </Box>
+      </ThemeProvider>
   );
 }
