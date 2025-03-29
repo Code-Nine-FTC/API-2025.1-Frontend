c8f4e0722fb085e62b6ad765b3ddfefa9731a333
Yuri Braga de Lima <79583088+yuribragga@users.noreply.github.com>
2025-03-29 13:43:56 -0300

    feat: mudança na tela de cadastro de estações

--- src/pages/RegisterStation.tsx
+++ src/pages/RegisterStation.tsx
@@ -1,24 +1,11 @@
 import { StationForm } from "@components/StationForm";
 import { LoggedLayout } from "@components/layout/layoutLogged";
-import axios from "axios";
+import { links } from "../services/api";
 
 const CadastrarEstacoes = () => {
   const handleCreate = async (form: any) => {
     try {
-      await axios.post("https://sua-api.com/weather_stations", {
-        name: form.name,
-        uid: form.uid,
-        address: {
-          zip: form.zip,
-          street: form.street,
-          number: form.number,
-          neighborhood: form.neighborhood,
-          city: form.city,
-          state: form.state,
-        },
-        latitude: form.latitude,
-        longitude: form.longitude,
-      });
+      await links.createStation(form);
       alert("Estação cadastrada com sucesso!");
     } catch (err) {
       alert("Erro ao cadastrar estação");
